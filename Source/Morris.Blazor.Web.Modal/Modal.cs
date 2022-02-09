7f7f6bfb309ce8a890d8d48e06f0c2862c28edd5
Peter Morris <mrpmorris@gmail.com>
2022-02-09 10:26:46 +0000

    Auto focus first available control when modal appears (#1)
    * Auto-focus

--- Source/Morris.Blazor.Web.Modal/Modal.cs
+++ Source/Morris.Blazor.Web.Modal/Modal.cs
@@ -1,32 +1,20 @@
 ﻿using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Rendering;
 
 namespace Morris.Blazor.Web.Modal
 {
 	public class Modal : ComponentBase, IDisposable
 	{
-		[Parameter]
-		public string? CssClass { get; set; }
-
-		[Parameter]
-		public Type? Layout { get; set; }
-
-		[Parameter]
-		public bool Visible { get; set; }
-
-		[Parameter]
-		public RenderFragment? ChildContent { get; set; }
-
-		[CascadingParameter]
-		private ModalHost ModalHost { get; set; } = null!;
-
-		[Parameter(CaptureUnmatchedValues = true)]
-		public Dictionary<string, object> Attributes { get; set; } = new(StringComparer.InvariantCultureIgnoreCase);
+		[Parameter] public string? CssClass { get; set; }
+		[Parameter] public Type? Layout { get; set; }
+		[Parameter] public bool Visible { get; set; }
+		[Parameter] public RenderFragment? ChildContent { get; set; }
+		[CascadingParameter] private ModalHost ModalHost { get; set; } = null!;
+		[Parameter(CaptureUnmatchedValues = true)] public Dictionary<string, object> AdditionalAttributes { get; set; } = new(StringComparer.InvariantCultureIgnoreCase);
 
 		public T? GetAttributeOrDefault<T>(string name)
 		{
 			ArgumentNullException.ThrowIfNull(name);
-			return (T?)Attributes.GetValueOrDefault(name);
+			return (T?)AdditionalAttributes.GetValueOrDefault(name);
 		}
 
 		protected override void OnInitialized()
---
+++ Source/Morris.Blazor.Web.Modal/wwwroot/scripts/index.js
@@ -0,0 +1,45 @@
+var MorrisBlazorWeb = MorrisBlazorWeb || {};
+
+MorrisBlazorWeb.canFocus = function (element) {
+	if (!(element.tabIndex >= 0) || element.disabled)
+		return false;
+
+	switch (element.nodeName) {
+		case 'A':
+			return element.rel != 'ignore';
+
+		case 'INPUT':
+			return element.type != 'hidden';
+
+		case 'BUTTON':
+		case 'SELECT':
+		case 'TEXTAREA':
+			return true;
+
+		default:
+			return false;
+	}
+}
+
+MorrisBlazorWeb.setFocus = function (element) {
+	if (!MorrisBlazorWeb.canFocus(element)) 
+		return false;
+
+	try {
+		element.focus();
+	} catch {
+	}
+	return document.activeElement === element;
+};
+
+MorrisBlazorWeb.focusFirstAvailableControl = function (element) {
+	if (MorrisBlazorWeb.setFocus(element))
+		return true;
+
+	for (var i = 0; i < element.childNodes.length; i++) {
+		var child = element.childNodes[i];
+		if (MorrisBlazorWeb.focusFirstAvailableControl(child))
+			return true;
+	}
+	return false;
+};
