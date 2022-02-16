1fa55cce0f1a1a3ad6d0ffd51797c069605fbb5d
Peter Morris <mrpmorris@gmail.com>
2022-02-16 10:31:05 +0000

    Stop modals recreating #4 (#5)

--- Source/Morris.Blazor.Web.Modal/ModalHost.cs
+++ Source/Morris.Blazor.Web.Modal/ModalHost.cs
@@ -13,9 +13,9 @@ namespace Morris.Blazor.Web.Modal
 
 		[Inject] private IJSRuntime JSRuntime { get; set; } = null!;
 
-		private ElementReference FieldsetElementReference;
-		private ElementReference ActiveModalElementReference;
 		private Modal? ActiveModal => !VisibleModals.Any() ? null : VisibleModals[^1];
+
+		private const string HolderElementId = "morris-blazor-web-modal_modal-holder";
 		private Modal? PreviouslyVisibleModal;
 		private ImmutableArray<Modal> VisibleModals = Array.Empty<Modal>().ToImmutableArray();
 
@@ -39,41 +39,31 @@ namespace Morris.Blazor.Web.Modal
 			// <CascadingValue>
 			{
 				builder.OpenComponent<CascadingValue<ModalHost>>(0);
-				builder.SetKey(this);
-				builder.AddAttribute(1, nameof(CascadingValue<ModalHost>.IsFixed), true);
-				builder.AddAttribute(2, nameof(CascadingValue<ModalHost>.Value), this);
-				builder.AddAttribute(3, nameof(CascadingValue<ModalHost>.ChildContent),
-					new RenderFragment(builder =>
-					{
-						ImmutableArray<Modal> modals = VisibleModals;
-						// <fieldset>
+				{
+					builder.SetKey(this);
+					builder.AddAttribute(1, nameof(CascadingValue<ModalHost>.IsFixed), true);
+					builder.AddAttribute(2, nameof(CascadingValue<ModalHost>.Value), this);
+					builder.AddAttribute(3, nameof(CascadingValue<ModalHost>.ChildContent),
+						new RenderFragment(builder =>
 						{
-							builder.OpenElement(4, "fieldset");
-							builder.AddAttribute(5, "id", "morris-blazor-web-modal_fieldset");
-							if (VisibleModals.Any())
-								builder.AddAttribute(6, "disabled");
-							builder.AddContent(7, ownChildContent);
-							builder.AddElementReferenceCapture(8, x => FieldsetElementReference = x);
-							RenderDisabledModals(9, modals, builder);
-							builder.CloseElement();
-						}
-						// </fieldset>
+							ImmutableArray<Modal> modals = VisibleModals;
 
-						if (VisibleModals.Any())
-						{
-							// <div>
+							builder.OpenElement(0, "section");
 							{
-								builder.OpenElement(10, "div");
-								builder.AddAttribute(11, "class", "modal_screen-obscurer");
-								builder.CloseElement();
+								builder.AddAttribute(1, "id", HolderElementId);
+								if (ActiveModal is not null)
+									builder.AddAttribute(2, "disabled", true);
+								builder.AddContent(3, ownChildContent);
+								RenderDisabledModals(4, modals, builder);
+								if (VisibleModals.Any())
+									RenderModal(builder, modals[^1], isActive: true);
 							}
-							// </div>
-							RenderModal(builder, modals[^1], isActive: true);
-						}
-					}));
-				builder.CloseComponent();
+							builder.CloseElement(); // section
+
+						}));
+				}
 			}
-			// </CascadingValue>
+			builder.CloseComponent(); // CascadingValue
 		}
 
 		protected override async Task OnAfterRenderAsync(bool firstRender)
@@ -83,60 +73,62 @@ namespace Morris.Blazor.Web.Modal
 			{
 				PreviouslyVisibleModal = ActiveModal;
 				if (ActiveModal is null)
-					await JSRuntime.InvokeVoidAsync("MorrisBlazorWeb.restoreControls", FieldsetElementReference);
+					await JSRuntime.InvokeVoidAsync("MorrisBlazorWeb.restoreControlsById", HolderElementId);
 				else
 				{
-					await JSRuntime.InvokeVoidAsync("MorrisBlazorWeb.disableControls", FieldsetElementReference);
-					await JSRuntime.InvokeVoidAsync("MorrisBlazorWeb.restoreControls", ActiveModalElementReference);
-					await JSRuntime.InvokeVoidAsync("MorrisBlazorWeb.focusFirstAvailableControl", ActiveModalElementReference);
+					await JSRuntime.InvokeVoidAsync("MorrisBlazorWeb.disableControlsById", HolderElementId);
+					await JSRuntime.InvokeVoidAsync("MorrisBlazorWeb.restoreControlsById", ActiveModal.Id);
+					await JSRuntime.InvokeVoidAsync("MorrisBlazorWeb.focusFirstAvailableControlById", ActiveModal.Id);
 				}
 			}
 		}
 
 		private void RenderDisabledModals(int index, ImmutableArray<Modal> modals, RenderTreeBuilder builder)
 		{
-			if (modals.Length <= 1)
-				return;
-
 			for (int i = 0; i < modals.Length - 1; i++)
 				RenderModal(builder, modals[i]);
+
+			if (VisibleModals.Any())
+			{
+				builder.OpenElement(index++, "div");
+				{
+					builder.AddAttribute(index++, "class", "modal_screen-obscurer");
+				}
+				builder.CloseElement(); // div
+			}
 		}
 
 		private void RenderModal(RenderTreeBuilder builder, Modal modal, bool isActive = false)
 		{
-			// <CascadingValue>
+			string modalIsActiveCssClass = isActive ? "modal-active" : "modal-inactive";
+			builder.OpenComponent<CascadingValue<Modal>>(0);
 			{
-				builder.OpenComponent<CascadingValue<Modal>>(0);
 				builder.SetKey(modal);
 				builder.AddAttribute(1, nameof(CascadingValue<Modal>.Value), modal);
 				builder.AddAttribute(2, nameof(CascadingValue<Modal>.IsFixed), true);
 				builder.AddAttribute(3, nameof(CascadingValue<Modal>.ChildContent),
 					new RenderFragment(builder =>
 					{
-						// <div>
+						builder.OpenElement(4, "div");
 						{
-							builder.OpenElement(4, "div");
-							builder.SetKey(modal);
-							builder.AddAttribute(5, "class", $"modal_container {modal.CssClass}");
-							builder.AddAttribute(6, "aria-modal", "true");
-							builder.AddAttribute(7, "role", "dialog");
-							builder.AddMultipleAttributes(8, modal.AdditionalAttributes);
-							// <LayoutView>
+							builder.SetKey(modal.Id);
+							builder.AddAttribute(5, "id", modal.Id);
+							builder.AddAttribute(6, "class", $"modal_container {modal.CssClass} {modalIsActiveCssClass}");
+							builder.AddAttribute(7, "aria-modal", "true");
+							builder.AddAttribute(8, "role", "dialog");
+							builder.AddMultipleAttributes(9, modal.AdditionalAttributes);
+
+							builder.OpenComponent<LayoutView>(10);
 							{
-								builder.OpenComponent<LayoutView>(9);
-								builder.AddAttribute(10, nameof(LayoutView.ChildContent), modal.ChildContent);
-								builder.AddAttribute(11, nameof(LayoutView.Layout), modal.Layout ?? DefaultModalLayout);
-								builder.CloseComponent();
+								builder.AddAttribute(0, nameof(LayoutView.ChildContent), modal.ChildContent);
+								builder.AddAttribute(1, nameof(LayoutView.Layout), modal.Layout ?? DefaultModalLayout);
 							}
-							if (isActive && AutoFocusActiveModal)
-								builder.AddElementReferenceCapture(12, x => ActiveModalElementReference = x);
-							builder.CloseElement();
+							builder.CloseComponent(); // LayoutView
 						}
-						// </div>
+						builder.CloseElement(); // div
 					}));
-				builder.CloseComponent();
 			}
-			// </CascadingValue>
+			builder.CloseComponent(); // CascadingValue
 		}
 	}
 }
