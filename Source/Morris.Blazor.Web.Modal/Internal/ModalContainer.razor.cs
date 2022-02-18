7112f9e977d260403e5d5f465cefbf2ea4cdd973
Peter Morris <mrpmorris@gmail.com>
2022-02-18 11:54:33 +0000

    * Make events in modals work #7

---
+++ Source/Morris.Blazor.Web.Modal/Internal/ModalContainer.razor.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Morris.Blazor.Web.Modal.Internal;
+
+public partial class ModalContainer : IDisposable
+{
+	[Parameter] public bool IsActive { get; set; }
+	[Parameter] public Modal? Modal { get; set; }
+	[Parameter] public RenderFragment? ChildContent { get; set; }
+	[CascadingParameter] public ModalHost? ModalHost { get; set; }
+
+	void IDisposable.Dispose()
+	{
+		if (Modal is not null)
+			ModalHost?.UnregisterContainerForModal(Modal);
+	}
+
+	internal void NotifyStateHasChanged()
+	{
+		StateHasChanged();
+	}
+
+	protected override void OnInitialized()
+	{
+		base.OnInitialized();
+		if (ModalHost is null)
+			throw new ArgumentNullException(nameof(ModalHost));
+		if (Modal is null)
+			throw new ArgumentNullException(nameof(Modal));
+		ModalHost.RegisterContainerForModal(Modal, this);
+	}
+
+	private string ActiveStatusCss => IsActive ? "modal-active" : "modal-inactive";
+
+}
